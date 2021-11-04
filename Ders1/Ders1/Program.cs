54709d6c678c8556cf318fcb6d6ef97de72e7eb7
Beytullah KALAY <beytullah.kalay@outlook.com>
2021-11-04 22:17:36 +0300

    Add: Ders1

---
+++ Ders1/Ders1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ders1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // HASTA OLDUGUM İCİN SORULARI VIDEOYU IZLEMEDEN COZDUM O YUZDEN 2 YANLIS :|
+           
+
+            Console.WriteLine("");
+            byte b = 5;
+            sbyte c = 5;
+
+            short s = 5;
+            ushort us = 5;
+
+            Int16 i16 = 2;
+            int i = 2;
+            Int32 i32 = 2;
+            Int64 i64 = 2;
+
+            uint ui = 2;
+            long l = 4;
+            ulong ul = 4;
+
+            float f = 5;
+            double d = 5;
+            decimal de = 5;
+
+            char ch = '2';
+            string str = "Beytullah";
+
+            bool b1 = true;
+            bool b2 = false;
+
+            DateTime dt = DateTime.Now;
+
+            Console.Write(dt);
+
+            object o1 = "x";
+            object o2 = 'y';
+            object o3 = 4;
+            object o4 = 4.3;
+
+
+            string str1 = string.Empty;
+            str1 = "Beytullah KALAY";
+
+
+        }
+    }
+}
