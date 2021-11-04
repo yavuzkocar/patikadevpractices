ec7607787c3c473326f964ce93c0288ceb80fed8
Beytullah KALAY <beytullah.kalay@outlook.com>
2021-11-04 13:00:49 +0300

    Add: Ders0

---
+++ Ders0/Ders0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ders0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Adiniz Soyadiniz: ");
+            name = Console.ReadLine();
+            Console.WriteLine(name);
+
+        }
+    }
+}
