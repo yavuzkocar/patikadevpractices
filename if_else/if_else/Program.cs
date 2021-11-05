742f486f1dcea1f0f82650fe69bf4234c39bb434
Beytullah KALAY <beytullah.kalay@outlook.com>
2021-11-05 09:31:49 +0300

    Add: if else

---
+++ if_else/if_else/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Gunaydin!");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("Iyi gunler!");
+            }
+            else
+            {
+                Console.WriteLine("Iyi geceler!");
+            }
+
+            string sonuc = time <= 18 ? "Iyi Günler" : "Iyi Geceler";
+
+            sonuc = time >= 6 && time < 11 ? "Gunaydin" : time <= 18 ? "Iyi Gunler" : "Iyi Geceler";
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
