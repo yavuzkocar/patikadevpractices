eecef078bc185d6a82860dda6009c941b3b6d6ba
Beytullah KALAY <beytullah.kalay@outlook.com>
2021-11-05 03:29:40 +0300

    Added: Try_Catch.

---
+++ Try_Catch/Try_Catch/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Console.WriteLine("Bir sayi giriniz : ");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Girilen sayi: " + sayi);
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Hata: " + ex.Message.ToString());
+            //    //throw;
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Islem tamamlandi.");
+            //}
+
+
+            try
+            {
+                //int a = int.Parse("test");
+                int b = int.Parse("-30000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Bos deger girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun degil");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Cok kucuk yada cok buyuk bir deger girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("islem basariyla tamamladi.");
+            }
+
+            int v = 20;
+            decimal h = v;
+
+        }
+    }
+}
